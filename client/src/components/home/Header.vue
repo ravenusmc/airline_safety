acd198f5e6a6e1cd06272ba685f3f8b496d2a5a4
ravenusmc <mcuddy77@gmail.com>
2019-03-26 20:05:11 -0400

    have header and footer set up for the project;

---
+++ client/src/components/home/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header>
+      <h1>Header</h1>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/components/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/home/Header';
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+  }
+}
+</script>
+
+<style>
+</style>
