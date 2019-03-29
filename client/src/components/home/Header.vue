b94328a5df9c8fab216a2ce87cbab26bad3ff371
ravenusmc <mcuddy77@gmail.com>
2019-03-28 20:07:46 -0400

    added text to header image

--- client/src/components/home/Header.vue
+++ client/src/components/home/Header.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
+
     <header>
+      <div class='header_div'>
+        <h1 class='center'>Airline Safety</h1>
+        <h4 class='center'>A Simple Study</h4>
+      </div>
     </header>
+
   </div>
 </template>
 
@@ -12,11 +18,28 @@ export default {
 </script>
 
 <style scoped>
+
+.center {
+  text-align: center;
+}
+
 header {
   background-image: url('../../assets/img/united.gif');
-  height: 300px;
+  height: 450px;
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
+  display: grid;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
+
+.header_div {
+  background-color: rgba(255, 0, 0, 0.5);
+  color: #fff;
+  border-radius: 10px;
+  padding: 20px;
+}
+
 </style>
