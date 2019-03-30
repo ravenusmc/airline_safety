15bab0f9407c8b393b39c94f150ee7677999d06d
ravenusmc <mcuddy77@gmail.com>
2019-03-30 15:02:20 -0400

    added vuex to project

---
+++ client/src/store/store.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+
+Vue.use(Vuex);
+
+const state = {
+};
+
+const getters = {
+
+};
+
+const actions = {
+
+};
+
+const mutations = {
+
+};
+
+export const store = new Vuex.Store({
+  modules: {
+    state,
+    getters,
+    actions,
+    mutations,
+  }
+});
